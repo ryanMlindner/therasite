3ce4408ac748d1cb8bd21042c32aa3d97aef7020
Ryan Lindner <ryan.m.lindner@gmail.com>
2024-06-17 10:39:58 -0600

    initial testing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RecoilRoot } from "recoil";
 import App from "./components/App";
 import "./css/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
