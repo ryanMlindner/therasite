f37683a21e13c4666151ed5ee5b1eda6acf44752
Ryan Lindner <ryan.m.lindner@gmail.com>
2024-07-23 12:56:37 -0600

    skeleton file structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Title from "./Displays/Title.js";
+import NavBar from "./Interacts/NavBar.js";
+import Home from "./Containers/Home.js";
 
 //TODO set up switches and routes
+/*
+<Route exact path="/test">
+  <Test/>
+</Route>
+*/
 export default function App() {
   return (
     <div className="App">
+      <Title/>
+      <NavBar/>
+      <Switch>
+        <Route exact path="/home">
+          <Home/>
+        </Route>
+      </Switch>
       <header className="App-header">
         <div>Placeholder!</div>
       </header>
