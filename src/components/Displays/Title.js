f04714e239795b3918a6f0854902c5db38c17851
Ryan Lindner <ryan.m.lindner@gmail.com>
2024-07-23 14:23:31 -0600

    title

--- src/components/Displays/Title.js
+++ src/components/Displays/Title.js
@@ -1,5 +1,10 @@
 export default function Title() {
   return (
-    <></>
+    <div className="title">
+      <div className="text">
+        <h4>White Elk Counseling LLC</h4>
+        <h5>Longmont, CO PLACEHOLDER 305.209.4581 Contact</h5>
+      </div>
+    </div>
   )
 }
