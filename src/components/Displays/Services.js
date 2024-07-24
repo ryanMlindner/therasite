a0c8525bced568723b8fc1c67b0892c665a1a57e
Ryan Lindner <ryan.m.lindner@gmail.com>
2024-07-24 13:15:09 -0600

    list display

--- src/components/Displays/Services.js
+++ src/components/Displays/Services.js
@@ -1,7 +1,21 @@
 export default function Displays() {
+  const services = ["Anxiety", "Depression", "Trauma", "PTSD", "Abuse Survivors",
+                    "Adjustment Disorder", "Agoraphobia", "Attachment Issues",
+                    "Bipolar Disorder", "Borderline Personality", "Chronic Illness",
+                    "Chronic Pain", "Couples Counseling", "Dissociation",
+                    "Dissociative Identity Disorder (DID)", "Domestic Violence",
+                    "Suicidal Ideation", "Spirituality and Religion",
+                    "Social Anxiety"]
   return (
     <div className="dark-background">
-
+      <div className="title">
+        <h5>Services</h5>
+      </div>
+      <div className="overflow-columns">
+        {services.map(service => {
+          return <div className="list-item">{service}</div>
+        })}
+      </div>
     </div>
   )
 }
